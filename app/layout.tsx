18b7d92150d4cdf42a1b740f4e0162fed12787ab
Selasie5 <selasisepenu5@gmail.com>
2025-12-03 15:16:32 +0200

    Fix font source path in layout.tsx for correct loading

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { Analytics } from "@vercel/analytics/next"
 import localFont from 'next/font/local';
 
 export const myLocalFont = localFont({
-  src: '/ppeditorialold-ultralight.woff2',
+  src: './ppeditorialold-ultralight.woff2',
   variable: '--font-local',
   display: 'swap',
 })
