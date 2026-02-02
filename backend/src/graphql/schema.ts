b9422055c3e9ddf4059b713cc2b332705e6ed114
Selasie5 <selasisepenu5@gmail.com>
2026-02-02 15:07:32 +0000

    Decouple backend

--- backend/src/graphql/schema.ts
+++
@@ -1,6 +0,0 @@
-import { authTypeDefs } from "./auth/schema";
-import { productTypeDefs } from "./product/schema";
-import { categoryTypeDefs } from "./category/schema";
-import { inventoryTypeDefs } from "./inventory/schema";
-
-export const typeDefs = [authTypeDefs, productTypeDefs, categoryTypeDefs, inventoryTypeDefs];
